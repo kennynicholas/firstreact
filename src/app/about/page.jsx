e19fc8108d9aafcda58895c6dbaa50db2c0a1f82
kenny <kennynicholas91@gmail.com>
2024-02-23 11:48:42 +0100

    this commit works

---
+++ src/app/about/page.jsx
@@ -0,0 +1,5 @@
+export default function About(){
+    return(
+        <h1 className="">This is the about page</h1>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+
+    <>
+    <h1>REACT NA CHAISE</h1>
+
+    <Link href="/about">Go to about page</Link>
+    </>
+    
+  );
+}
